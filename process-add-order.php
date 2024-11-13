a94c3ae2d7b258bcd74fd20d4e1454421b40a177
Moul Sàn <94727379+moulsan369@users.noreply.github.com>
2024-11-13 11:36:35 +0700

    Up to date

--- process-add-order.php
+++ process-add-order.php
@@ -1,10 +1,10 @@
 <?php
 session_start();
 require 'connect.php';
-$sqlCountOrder = "SELECT * FROM donorder ORDER BY MaDon DESC LIMIT 1";
+$sqlCountOrder = "SELECT * FROM dondatnuoc ORDER BY MADON DESC LIMIT 1";
 $result = mysqli_query($connect,$sqlCountOrder);
 $numOrder = mysqli_fetch_assoc($result);
-preg_match('/\d+/', $numOrder['MaDon'], $numbersArray);
+preg_match('/\d+/', $numOrder['MADON'], $numbersArray);
 $number = $numbersArray[0];
 $number++;
 $ORD_ID = "D".sprintf("%04d",$number);
@@ -12,7 +12,7 @@ $ORD_ID = "D".sprintf("%04d",$number);
 // insert order
 $soBan = $_SESSION['tableNumber'];
 $currentDateTime = date('Y-m-d H:i:s');
-$sql = "INSERT INTO `donorder` VALUES('$ORD_ID','$soBan','$currentDateTime','Chưa pha chế')";
+$sql = "INSERT INTO `dondatnuoc` VALUES('$ORD_ID','$soBan','$currentDateTime','Chưa pha chế')";
 mysqli_query($connect,$sql);
 
 // insert ct order
@@ -20,7 +20,7 @@ foreach ($_SESSION['order'] as $each) {
     $MaDoUong = $each['MaDoUong'];
     $SoLuong = $each['SoLuong'];
     $orderNote = $each['OrderNote'];
-    $sql = "INSERT INTO `ctdonorder` VALUES('$ORD_ID','$MaDoUong','$SoLuong','$orderNote')";
+    $sql = "INSERT INTO `ct_dondatnuoc` VALUES('$ORD_ID','$MaDoUong','$SoLuong','$orderNote')";
     mysqli_query($connect,$sql);
 }
 
